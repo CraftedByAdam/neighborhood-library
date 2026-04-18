6bf1d40cbe6cb69d6289bc717de25825c97ef59a
Your Name <youremail@example.com>
2026-04-18 17:48:00 -0400

    I made a Switch to hold 3 of the prompts I ask of the user, as well as a default to say "Invalid Choice" for safety. I also made one method outside the main to list all the books and an input for the user to enter the Id of the book they want to check out with a choice to cancel and go back to the home screen. I did not finish the listAllBooks method yet and I will continue to work on it

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -9,38 +9,68 @@ public class NeighborhoodLibrary {
     private static int numOfBooks = 20;
 
 
-    public  static void main(String[] args) {
-        books[0] = new Book(1, "978-0590846288", "Adam Becomes an Engineer - Hello World");
-        books[1] = new Book(2, "978-0590846288", "Adam Becomes an Engineer Chapter 2");
-        books[2] = new Book(3, "978-0590846288", "Adam Becomes an Engineer Chapter 3");
-        books[3] = new Book(4, "978-0590846288", "Adam Becomes an Engineer Chapter 4");
-        books[4] = new Book(5, "978-0590846288", "Adam Becomes an Engineer - Stuck in a Loop");
-        books[5] = new Book(6, "978-0590846288", "Adam Becomes an Engineer Chapter 6");
-        books[6] = new Book(7, "978-0590846288", "Adam Becomes an Engineer Chapter 7");
-        books[7] = new Book(8, "978-0590846288", "Adam Becomes an Engineer Chapter 8");
-        books[8] = new Book(9, "978-0590846288", "Adam Becomes an Engineer Chapter 9");
-        books[9] = new Book(10, "978-0590846288", "Adam Becomes an Engineer - Fighting Bugs");
-        books[10] = new Book(11, "978-0590846288", "Adam Becomes an Engineer Chapter 11");
-        books[11] = new Book(12, "978-0590846288", "Adam Becomes an Engineer Chapter 12");
-        books[12] = new Book(13, "978-0590846288", "Adam Becomes an Engineer Chapter 13");
-        books[13] = new Book(14, "978-0590846288", "Adam Becomes an Engineer Chapter 14");
-        books[14] = new Book(15, "978-0590846288", "Adam Becomes an Engineer - Unknown Errors");
-        books[15] = new Book(16, "978-0590846288", "Adam Becomes an Engineer Chapter 16");
-        books[16] = new Book(17, "978-0590846288", "Adam Becomes an Engineer Chapter 17");
-        books[17] = new Book(18, "978-0590846288", "Adam Becomes an Engineer Chapter 18");
-        books[18] = new Book(19, "978-0590846288", "Adam Becomes an Engineer Chapter 19");
-        books[19] = new Book(20, "978-0590846288", "Adam Becomes an Engineer - The Last Line");
+    public static void main(String[] args) {
+        books[0] = new Book(1, "111-1111111101", "Adam Becomes an Engineer - Hello World!");
+        books[1] = new Book(2, "111-1111111102", "Adam Becomes an Engineer Chapter 2");
+        books[2] = new Book(3, "111-1111111103", "Adam Becomes an Engineer Chapter 3");
+        books[3] = new Book(4, "111-1111111104", "Adam Becomes an Engineer Chapter 4");
+        books[4] = new Book(5, "111-1111111105", "Adam Becomes an Engineer - Stuck in a Loop!");
+        books[5] = new Book(6, "111-1111111106", "Adam Becomes an Engineer Chapter 6");
+        books[6] = new Book(7, "111-1111111107", "Adam Becomes an Engineer Chapter 7");
+        books[7] = new Book(8, "111-1111111108", "Adam Becomes an Engineer Chapter 8");
+        books[8] = new Book(9, "111-1111111109", "Adam Becomes an Engineer Chapter 9");
+        books[9] = new Book(10, "111-11111111010", "Adam Becomes an Engineer - Fighting Bugs!");
+        books[10] = new Book(11, "111-1111111011", "Adam Becomes an Engineer Chapter 11");
+        books[11] = new Book(12, "111-1111111012", "Adam Becomes an Engineer Chapter 12");
+        books[12] = new Book(13, "111-1111111013", "Adam Becomes an Engineer Chapter 13");
+        books[13] = new Book(14, "111-1111111014", "Adam Becomes an Engineer Chapter 14");
+        books[14] = new Book(15, "111-1111111015", "Adam Becomes an Engineer - Unknown Errors?");
+        books[15] = new Book(16, "111-1111111016", "Adam Becomes an Engineer Chapter 16");
+        books[16] = new Book(17, "111-1111111017", "Adam Becomes an Engineer Chapter 17");
+        books[17] = new Book(18, "111-1111111018", "Adam Becomes an Engineer Chapter 18");
+        books[18] = new Book(19, "111-1111111019", "Adam Becomes an Engineer Chapter 19");
+        books[19] = new Book(20, "111-1111111020", "Adam Becomes an Engineer - The Last Line!");
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Store Home Screen");
-        System.out.println("------------------");
-        System.out.println("1. Show Available Books");
-        System.out.println("2. Show checked Out Books");
-        System.out.println("3. Exit");
-        System.out.print("Enter your choice (1-3): ");
-        int choice = scanner.nextInt();
+        boolean isDone = false;
+        while (!isDone) {
 
-        scanner.nextLine();
+            System.out.println("Store Home Screen");
+            System.out.println("------------------");
+            System.out.println("1. Show Available Books");
+            System.out.println("2. Show checked Out Books");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice (1-3): ");
+            int choice = scanner.nextInt();
+
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    listAllBooks(scanner);
+                    break;
+                case 2:
+
+                case 3:
+
+                default:
+                    System.out.println("Invalid Choice");
+                    break;
+            }
+        }
+    }
+
+    public static void listAllBooks(Scanner scanner) {
+        System.out.println("Books Available");
+        for (int i = 0; i < numOfBooks; i++) {
+            System.out.println(books[i]);
+        }
+
+        System.out.println("------------------");
+
+        System.out.print("Enter the Id of the book you want to check out (0 to cancel): ");
+        String bookId = scanner.next();
+
     }
 }
